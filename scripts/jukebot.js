af0df230db5e02dccbd33357a6d2a1c413a1e5d6
Roman Shterenzon <roman.shterenzon@gmail.com>
2016-09-28 11:37:32 +0300

    Initial commit

---
+++ scripts/jukebot.js
@@ -0,0 +1,67 @@
+var Mopidy = require('mopidy');
+var CONNECT_TIMEOUT = 5000;
+var host = process.env.MOPIDY_HOST || '127.0.0.1';
+
+function trackDesc(track) {
+  if (!track) {
+    return 'nothing';
+  }
+  if (track.artists) {
+    return track.name + ' by ' + track.artists[0].name + ' from ' + track.album.name;
+  } else {
+    return track.name + ' (' + track.album.name + ')';
+  }
+}
+
+// var get = (key, object) => object[key];
+
+var state = {
+  searchResult: null
+};
+
+module.exports = function(bot) {
+  var mopidy = new Mopidy({
+    webSocketUrl: 'ws://' + host + ':6680/mopidy/ws/'
+  });
+  // mopidy.on(console.log.bind(console));
+  new Promise(function(resolve, reject) {
+    mopidy.on('state:online', resolve);
+    setTimeout(reject, CONNECT_TIMEOUT);
+  }).then(defineCommands, function() {
+    console.error('Unable to connect to mopidy');
+    mopidy.off();
+    mopidy.close();
+  });
+
+  function defineCommands() {
+    bot.respond(/search\s+(.*)/i, function(res) {
+      var query = res.match[1];
+      console.log('searching for ' + query);
+      mopidy.library.search({
+        any: [query]
+      }).then(function(result) {
+        result = result[0];
+        state.searchResult = result;
+        var msg = result.tracks.map(trackDesc).map((str, i) => i + 1 + '. ' + str).join("\n");
+        res.reply(msg);
+      });
+    });
+
+    bot.respond(/play\s+(\d+)/i, function(res) {
+      var i = parseInt(res.match[1], 10),
+        track;
+      if (state.searchResult && (track = state.searchResult.tracks[i])) {
+        mopidy.tracklist.add([track]).then( (tl) => mopidy.playback.play(tl[0]) );
+        res.reply("Going to play " + trackDesc(track));
+      } else {
+        res.reply("I don't know what to play");
+      }
+    });
+
+    bot.respond(/what's playing\?/i, function(res) {
+      mopidy.playback.getCurrentTrack().then(function(track) {
+        res.reply("Currently playing " + trackDesc(track));
+      });
+    });
+  }
+};
